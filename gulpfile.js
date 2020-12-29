a8947c834a6be567104ba75a9d55b84633e08f68
Kai Shoo <qsteamk@gmail.com>
2020-12-30 02:20:20 +0300

    added mobile no-js header

--- gulpfile.js
+++ gulpfile.js
@@ -153,7 +153,7 @@ exports.reload = reload;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles, reload));
   gulp.watch("source/js/*.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 };
