c6a35d6316906d707b0c0759628cc6099eba44db
Kai Shoo <qsteamk@gmail.com>
2021-01-25 20:23:36 +0300

    refactored header added fixed header

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,8 +1,11 @@
 "use strict";
 
+const headerWrapper = document.querySelector(".page__header");
+
 const header = document.querySelector(".page-header");
 const toggle = document.querySelector(".page-header__toggle");
-const wrapper = document.querySelector(".wrapper--header-inactive");
+
+const navHeight = header.getBoundingClientRect().height;
 
 const init = function () {
   header.classList.toggle("page-header--no-js");
@@ -14,8 +17,6 @@ init();
 const doToggle = function () {
   header.classList.toggle("page-header--active");
   header.classList.toggle("page-header--inactive");
-  wrapper.classList.toggle("wrapper--header-active");
-  wrapper.classList.toggle("wrapper--header-inactive");
 };
 
 toggle.addEventListener("click", function (e) {
@@ -24,10 +25,30 @@ toggle.addEventListener("click", function (e) {
   doToggle();
 });
 
+// KEYS , MOUSE EXITS
+
 document.addEventListener("click", function (e) {
+  e.preventDefault();
+
   if (header.classList.contains("page-header--active")) {
     if (!e.target.closest(".page-header")) {
       doToggle();
     }
   }
 });
+
+// STICKY NAV
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) header.classList.add("page-header--fixed");
+  else header.classList.remove("page-header--fixed");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0.99,
+});
+
+headerObserver.observe(headerWrapper);
